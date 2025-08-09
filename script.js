0d48370b29672571eca66ff4f084d1e779b9cb82
Adriano <adrianosouzascj@gmail.com>
2025-08-09 01:19:35 -0300

    projeto funcionando

--- script.js
+++ script.js
@@ -13,11 +13,23 @@ reset();
 // Events
 document.querySelector('.reset').addEventListener('click', reset);
 
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick)
 
+});
 
 
 
 // Functions
+
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === '') {
+      square[item] = player;
+      renderSquare();
+      togglePlayer();
+    }
+}
 
 function reset() {
     warning = '';
@@ -38,6 +50,66 @@ function reset() {
 
 function renderSquare() {
     for (let i in square) {
-        console.log(`Item: ${i}`);
+        let item = document.querySelector(`div[data-item="${i}"]`);
+        item.innerHTML = square[i];
     }
+
+    checkGame();
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = player;
+    document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+    player = (player === 'X') ? 'O' : 'X';
+    renderInfo();
+
+}
+
+function checkGame() {
+    if(checkWinnerFor('X')) {
+      warning = 'O "X" venceu!';
+      playing = false;
+    } else if(checkWinnerFor('O')) {
+      warning = 'O "O" venceu!';
+      playing = false;
+    } else if(isFull()) {
+      warning = 'Deu empate!';
+      playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+    let pos = [
+      'a1,a2,a3',
+      'b1,b2,b3',
+      'c1,c2,c3',
+      'a1,b1,c1',
+      'a2,b2,c2',
+      'a3,b3,c3',
+      'a1,b2,c3',
+      'a3,b2,c1'
+    ];
+    
+    for(let w in pos) {
+        let pArray = pos[w].split(',');
+        let hasWon = pArray.every((option)=> square[option] === player);
+        if(hasWon) {
+            return true;
+        } 
+    }
+
+    return false;
+
+}
+
+function isFull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+    return true;
 }
